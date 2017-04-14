458b3734da39994f0126c25907be4dbf1dbecf07
jejessica <jmd.sants@gmail.com>
2017-04-14 12:53:48 -0300

    Criada as classes:BeansMedico, BeansPaciente, BeansUsuario

---
+++ ConsultorioMedico/src/modeloBean/BeansPaciente.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modeloBean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Marcus
+ */
+public class BeansPaciente {
+    private int id_pct;
+    private String nome;
+    private String rg;
+    private String cpf;
+    private Date dataNascimento;
+    private String telefoneRes;
+    private String telefoneCel;
+    private String observacao;
+
+    public int getId_pct() {
+        return id_pct;
+    }
+
+    public void setId_pct(int id_pct) {
+        this.id_pct = id_pct;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefoneRes() {
+        return telefoneRes;
+    }
+
+    public void setTelefoneRes(String telefoneRes) {
+        this.telefoneRes = telefoneRes;
+    }
+
+    public String getTelefoneCel() {
+        return telefoneCel;
+    }
+
+    public void setTelefoneCel(String telefoneCel) {
+        this.telefoneCel = telefoneCel;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    
+    
+}
---
+++ ConsultorioMedico/src/modeloBean/BeansUsuario.java
@@ -0,0 +1,87 @@
+
+package modeloBean;
+
+/**
+ * @author Jessica
+ */
+
+public class BeansUsuario {
+    
+    private Integer cd_usuario;
+    private String nm_usuario;
+    private String ds_senha;  
+    private String tipo_usuario;    
+    private String pesquisaUsuario;
+
+    /**
+     * @return the cd_usuario
+     */
+    public int getCd_usuario() {
+        return cd_usuario;
+    }
+
+    /**
+     * @param cd_usuario the cd_usuario to set
+     */
+    public void setCd_usuario(int cd_usuario) {
+        this.cd_usuario = cd_usuario;
+    }
+
+    /**
+     * @return the nm_usuario
+     */
+    public String getNm_usuario() {
+        return nm_usuario;
+    }
+
+    /**
+     * @param nm_usuario the nm_usuario to set
+     */
+    public void setNm_usuario(String nm_usuario) {
+        this.nm_usuario = nm_usuario;
+    }
+
+    /**
+     * @return the ds_senha
+     */
+    public String getDs_senha() {
+        return ds_senha;
+    }
+
+    /**
+     * @param ds_senha the ds_senha to set
+     */
+    public void setDs_senha(String ds_senha) {
+        this.ds_senha = ds_senha;
+    }
+
+    /**
+     * @return the tipo_usuario
+     */
+    public String getTipo_usuario() {
+        return tipo_usuario;
+    }
+
+    /**
+     * @param tipo_usuario the tipo_usuario to set
+     */
+    public void setTipo_usuario(String tipo_usuario) {
+        this.tipo_usuario = tipo_usuario;
+    }
+
+    /**
+     * @return the pesquisaUsuario
+     */
+    public String getPesquisaUsuario() {
+        return pesquisaUsuario;
+    }
+
+    /**
+     * @param pesquisaUsuario the pesquisaUsuario to set
+     */
+    public void setPesquisaUsuario(String pesquisaUsuario) {
+        this.pesquisaUsuario = pesquisaUsuario;
+    }
+   
+    
+}
